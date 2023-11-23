27346a2623c8b7812a64bd68ce530d9b03da2240
ricardoaugusto1210 <ricardoaugusto.eb@gmail.com>
2023-11-23 15:04:04 -0300

    add route

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Saiu novo filme",
+    "description": "Novo filme.",
+    "photoCover":"https://f.i.uol.com.br/fotografia/2021/05/12/1620835470609bfc8e766b4_1620835470_3x2_md.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Nova série anunciada pela Disney+",
+    "description": "bla bla bla",
+    "photoCover":"https://classic.exame.com/wp-content/uploads/2021/09/Shang-Chi-Estreia-1024x576-1.jpg?quality=70&strip=info&w=1024"
+
+  }
+]
